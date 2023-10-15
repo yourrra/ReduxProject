09383ef291538c59ac2f1db3483ad70c6bb2ff4b
yourrra <yuri15616@gmailcom>
2023-10-16 01:00:17 +0300

    style: remove comments

--- src/asyncAction/customer.ts
+++ src/asyncAction/customer.ts
@@ -19,27 +19,3 @@ export const fetchCustomers = () => {
     }
   }
 }
-
-// export const fetchCustomers = async (dispatch: Dispatch) => {
-//   try {
-//     const response = await axios.get<User[]>(
-//       'https://jsonplaceholder.typicode.com/users',
-//     )
-//     dispatch(addManyCustomerAction(response.data))
-//   } catch (error) {
-//     console.error('Error fetching customers: ', error)
-//   }
-// }
-
-// export const fetchCustomers = async (dispatch: Dispatch) => {
-//     try {
-//       const response = await fetch('https://jsonplaceholder.typicode.com/users')
-//       if (!response.ok) {
-//         throw new Error('Failed to fetch customers')
-//       }
-//       const data = await response.json()
-//       dispatch(addManyCustomerAction(data))
-//     } catch (error) {
-//       console.error('Error fetching customers: ', error)
-//     }
-//   }
